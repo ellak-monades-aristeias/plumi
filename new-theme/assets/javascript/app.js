c90cfe757ed5c5a3f8f4c5c0c518c296b3447fd3
Yiannis St <jiannst@gmail.com>
2015-09-26 22:30:24 +0300

    Added 4 more templates + Various Improvements

--- new-theme/assets/javascript/app.js
+++ new-theme/assets/javascript/app.js
@@ -1,15 +1,30 @@
 (function() {
 
-	(function($) {
-		
-		jQuery('.dropdown-toggle').dropdown();
+    (function($) {
 
-		jQuery('#main-video').mediaelementplayer({
-            success: function (mediaElement, domObject) {
+        jQuery('.dropdown-toggle').dropdown();
+
+        jQuery('#main-video').mediaelementplayer({
+            success: function(mediaElement, domObject) {
 
             }
         });
-		
-	}(jQuery));
+
+        jQuery('.show-more-text a').on("click", function(e) {
+            e.preventDefault();
+            var a = jQuery(this).parent().parent();
+
+            if (a.hasClass("show-the-text")) {
+                a.find('.more-text').slideUp(300, function() {
+                    a.toggleClass("show-the-text");
+                });
+            } else {
+                a.find('.more-text').slideDown(300, function() {
+                    a.toggleClass("show-the-text");
+                });
+            }
+        });
+
+    }(jQuery));
 
 })();
