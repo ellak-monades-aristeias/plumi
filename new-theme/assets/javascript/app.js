69a3092ed61a8bb65035b4edd7f51d17a0e0263f
Yiannis St <jiannst@gmail.com>
2015-10-01 00:29:36 +0300

    Added search and countries category pages

--- new-theme/assets/javascript/app.js
+++ new-theme/assets/javascript/app.js
@@ -1,30 +1,51 @@
 (function() {
 
-    (function($) {
+	(function($) {
+		jQuery('.dropdown-toggle').dropdown();
 
-        jQuery('.dropdown-toggle').dropdown();
-
-        jQuery('#main-video').mediaelementplayer({
-            success: function(mediaElement, domObject) {
+		jQuery('#main-video').mediaelementplayer({
+            success: function (mediaElement, domObject) {
 
             }
         });
 
-        jQuery('.show-more-text a').on("click", function(e) {
+        jQuery('.show-more-text a').on("click", function(e){
             e.preventDefault();
             var a = jQuery(this).parent().parent();
-
-            if (a.hasClass("show-the-text")) {
-                a.find('.more-text').slideUp(300, function() {
+            
+            if( a.hasClass("show-the-text") ){
+                a.find('.more-text').slideUp(300, function(){
                     a.toggleClass("show-the-text");
                 });
-            } else {
-                a.find('.more-text').slideDown(300, function() {
+            }
+            else{
+                a.find('.more-text').slideDown(300, function(){
                     a.toggleClass("show-the-text");
                 });
             }
         });
 
-    }(jQuery));
+        jQuery('.results-filter .dropdown-menu').on('click', function(event){
+            var events = $._data(document, 'events') || {};
+            events = events.click || [];
+            for(var i = 0; i < events.length; i++) {
+                if(events[i].selector) {
+
+                    //Check if the clicked element matches the event selector
+                    if($(event.target).is(events[i].selector)) {
+                        events[i].handler.call(event.target, event);
+                    }
+
+                    // Check if any of the clicked element parents matches the 
+                    // delegated event selector (Emulating propagation)
+                    $(event.target).parents(events[i].selector).each(function(){
+                        events[i].handler.call(this, event);
+                    });
+                }
+            }
+            event.stopPropagation(); //Always stop propagation
+        });
+        
+	}(jQuery));
 
 })();
